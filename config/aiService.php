48a7c6702728747fd73d5c6ccb8bb1dd73bb9b6b
Masaki Kawabata Neto <kawabata@usp.br>
2026-09-08 07:57:22 -0300

    feat: tempo de cache configuravel

--- config/aiService.php
+++ config/aiService.php
@@ -1,9 +1,18 @@
 <?php
 
 return [
-    'litellm' => [
-        'url' => env('LITELLM_URL'),
-        'key' => env('LITELLM_API_KEY'),
-        'model' => env('LITELLM_MODEL'),
+    # Provedor de IA padrão
+    'defaultProvider' => env('AI_PROVIDER', 'litellm'),
+
+    'providers' => [
+        'litellm' => [
+            'url' => env('LITELLM_URL'),
+            'key' => env('LITELLM_API_KEY'),
+            'model' => env('LITELLM_MODEL'),
+        ],
     ],
+
+    # tempo de retenção do cache local em segundos (padrão: 604800 = 7 dias)
+    'cacheRetention' => (int) env('AI_CACHE_RETENTION', '604800'),
+
 ];
